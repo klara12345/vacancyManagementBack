3123f6200e53caef988b50003fc8bff6d6534e93
alanaev <Eva.Nako@raiffeisen.al>
2022-02-10 16:10:10 +0100

    API CRUD

---
+++ VacancyManagment/DTO/CandidateDTO.cs
@@ -0,0 +1,19 @@
+﻿using VacancyManagment.Models;
+
+namespace VacancyManagment.DTO
+{
+    public class CandidateDTO
+    {
+        public string? CandidateName { get; set; }
+        public string? CandidateSurname { get; set; }
+        public string? CandidateMobile { get; set; }
+        public string? CandidateEmail { get; set; }
+        public byte[]? CandidateCv { get; set; }
+        public CandidateDTO() { }
+        public CandidateDTO(Candidate candidateItem) =>
+            (CandidateName, CandidateSurname, CandidateMobile, CandidateEmail, CandidateCv) =
+            (candidateItem.CandidateName, candidateItem.CandidateSurname, candidateItem.CandidateMobile, candidateItem.CandidateEmail, candidateItem.CandidateCv);
+       
+
+    }
+}
--- VacancyManagment/Program.cs
+++ VacancyManagment/Program.cs
@@ -44,7 +44,33 @@ async([FromBody] VacanciesDTO vacancydto,[FromServices] VacancyCadidatesContext 
     return Results.Created($"/GetVacancy/{vacancy.IdVacancy}", new VacanciesDTO(vacancy));
 });
 
-     
+app.MapPut("/vacancies/{id}",
+    async (long id, VacanciesDTO vacancydto, VacancyCadidatesContext context) =>
+    {
+        var vacancy = await context.Vacancies.FindAsync(id);
+        if (vacancy == null) return Results.NotFound();
+        vacancy.Vacancy1 = vacancydto.Vacancy1;
+        vacancy.Description = vacancydto.Description;
+        vacancy.IsActive = vacancydto.IsActive;
+        vacancy.DateCreated = vacancydto.DateCreated;
+        vacancy.DateSaved = vacancydto.DateSaved;
+        await context.SaveChangesAsync();
+        return Results.NoContent();
+    });
+
+app.MapDelete("/vacancies/{id}",
+    async (long id, VacancyCadidatesContext context) =>
+    {
+        if(await context.Vacancies.FindAsync(id) is Vacancy vacancy)
+        {
+            context.Vacancies.Remove(vacancy);
+            await context.SaveChangesAsync();
+            return Results.Ok(new VacanciesDTO(vacancy));
+        }
+        return Results.NotFound();
+    });
+
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
